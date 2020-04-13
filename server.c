a6261b0be54afd38687fcd6d74b1beebc1d32826
Fedor Chervyakov <fedor.chervyakov@emlid.com>
2020-04-13 15:42:05 +0300

    server: add Unix socket init/deinit, update cli, remove file open/close
    main:
    - Remove calls to open/close and related code
    - Add LISTEN_BACKLOG define
    - Add error_handler macro
    - Add listening socket initialization code, i.e socket address
    structure initialization, and successive calls to socket(), bind(),
     and listen().
    - Add socket deinit, i.e. calls to unlink() and close().
    - Add a dummy connection handler (accept()) that does nothing.
    
    cli:
    - Rename version and print functions to print_version and print_cli to
    better represent the purpose of each.
    - Fix usage message, the arguments to printf were in incorrect order.
    - Change the first positional argument to specify the socket path,
    since now working with sockets. Update usage message correspondingly.

--- server.c
+++ server.c
@@ -10,35 +10,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <string.h>
 
 #include "pingpong.h"
 
 
 #define PROGRAM_NAME "pp-server"
-
 #define OPTIONS "hv"
 
-static char *filename;
+#define LISTEN_BACKLOG 5
 
-static void version (void);
-static void usage (void);
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+
+static char *sock_path;
+
+
+static void print_version (void);
+static void print_usage (void);
 static void parse_options (int argc, char **argv);
 int main (int argc, char **argv);
 
 
 static void
-version (void)
+print_version (void)
 {
     printf("%s: version %d.%d \n", PROGRAM_NAME, VERSION_MAJOR, VERSION_MINOR);
     return;
 }
 
 static void
-usage (void)
+print_usage (void)
 {
-    printf("%s: usage \n\t./%s [-%s] filename\n",
-           PROGRAM_NAME, OPTIONS, PROGRAM_NAME
+    printf("%s: usage \n./%s [-%s] sock_path\n",
+           PROGRAM_NAME, PROGRAM_NAME, OPTIONS
            );
     return;
 }
@@ -51,10 +57,10 @@ parse_options (int argc, char **argv)
     while ((c = getopt(argc, argv, OPTIONS)) != -1) {
         switch (c) {
             case 'h':
-                usage();
+                print_usage();
                 exit(EXIT_SUCCESS);
             case 'v':
-                version();
+                print_version();
                 exit(EXIT_SUCCESS);
             default:
                 goto exit_failure;
@@ -62,10 +68,10 @@ parse_options (int argc, char **argv)
     }
 
     if (optind == argc-1) {
-        filename = argv[optind];
-        printf("filename: %s\n", filename);
+        sock_path = argv[optind];
+        printf("sock_path: %s\n", sock_path);
     } else if (optind == argc) {
-        printf("%s: missing file operand\n", PROGRAM_NAME);
+        printf("%s: missing socket path\n", PROGRAM_NAME);
         goto exit_failure;
     } else {
         printf("%s: two many arguments\n", PROGRAM_NAME);
@@ -79,20 +85,45 @@ exit_failure:
     exit(EXIT_FAILURE);
 }
 
-
 int
 main (int argc, char **argv)
 {
-    int fd;
+    int sfd, cfd;
+    struct sockaddr_un my_addr;
 
     parse_options(argc, argv);
 
-    if ((fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
-        printf("%s: Failed to open %s\n", PROGRAM_NAME, filename);
-        exit(fd);
-    }
+    /* Create Unix socket */
+    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sfd == -1)
+        handle_error("socket");
 
-    close(fd);
+    /* Initialize my_addr structure with pathname set to sock_path */
+    memset(&my_addr, 0, sizeof(struct sockaddr_un)); /* clear structure */
+    my_addr.sun_family = AF_UNIX;
+    strncpy(my_addr.sun_path, sock_path, sizeof(my_addr.sun_path)-1);
+
+    /* Bind socket to the address */
+    if (bind(sfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_un)) == -1)
+        handle_error("bind");
+
+    /* Mark socket as passive socket */
+    if (listen(sfd, LISTEN_BACKLOG) == -1)
+        handle_error("listen");
+
+    /* Accept an incoming connection */
+
+    cfd = accept(sfd, NULL, NULL);
+    if (cfd == -1)
+        handle_error("accept");
+
+
+    /* Grace exit */
+    if (unlink(sock_path) == -1)
+        handle_error("unlink");
+
+    if (close(sfd) == -1)
+        handle_error("close");
 
     exit(EXIT_SUCCESS);
 }
