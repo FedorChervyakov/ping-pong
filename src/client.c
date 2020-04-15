3b40c92a959ffe775ef6b785e4271887f946514c
Fedor Chervyakov <fedor.chervyakov@emlid.com>
2020-04-15 19:43:49 +0300

    client: add long options, use env var as bind path, add protocol check
    client.c:
    - Replace getopt with getopt_long, add long options for existing ones.
    - Add -u or --unix option with an optional argument to specify the use
    of Unix socket. If no argument is specified, check whether the
    UNIX_SOCKET environmental variable is set, use its value as path
    to bind the socket to, if not set use the default path UNIX_SOCKET_PATH
    specified in pingpong.h. Otherwise, use the argument as path to bind to.
    - Add protocol enum, and variable which is set during the option parsing
    to specified protocol. If no protocol is specified exit with error.
    - Update usage message to accomodate long options

--- src/client.c
+++ src/client.c
@@ -14,19 +14,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 #include "pingpong.h"
 
 
 #define PROGRAM_NAME "pp-client"
-#define OPTIONS "hv"
 
 
-static char *sock_path;
+static char *short_options = "hvu::";
+static struct option long_options[] = {
+    {"unix",    optional_argument, 0, 'u'},
+    {"help",    no_argument,       0, 'h'},
+    {"version", no_argument,       0, 'v'},
+    {0,         0,                 0, 0}
+};
+
+enum protocol
+{
+    NONE,
+    UNIX,
+};
+
+static enum protocol proto = NONE;
+
+static char unix_sock_path[108] = UNIX_SOCKET_PATH;
 
 static void print_version (void);
 static void print_usage (void);
 static void parse_options (int argc, char **argv);
+static void parse_unix_socket_param (const char *optarg);
 int main (int argc, char **argv);
 
 
@@ -40,10 +57,32 @@ print_version (void)
 static void
 print_usage (void)
 {
-    printf("%s: usage \n./%s [-%s] sock_path\n",
-           PROGRAM_NAME, PROGRAM_NAME, OPTIONS
-           );
+    printf("%s: usage \n./%s [-hvu] [-u sock_path]\n",
+           PROGRAM_NAME, PROGRAM_NAME);
+    puts(("\
+\t-h, --help              show this message\n\
+\t-u, --unix [pathname]   use Unix socket, bind it to pathname\n\
+\t-v, --version           show version"));
     return;
+}
+
+
+static void
+parse_unix_socket_param (const char *optarg)
+{
+    char *env_var;
+
+    if (! optarg) {
+        if ((env_var = getenv("UNIX_SOCKET")) != NULL) {
+            /* Use env var if set */
+            strcpy(unix_sock_path, env_var);
+        }
+        return;
+    }
+
+    strcpy(unix_sock_path, optarg);
+    return;
+
 }
 
 static void
@@ -51,7 +90,9 @@ parse_options (int argc, char **argv)
 {
     int c;
 
-    while ((c = getopt(argc, argv, OPTIONS)) != -1) {
+    while ((c = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+        char *temp_arg = NULL;
+
         switch (c) {
             case 'h':
                 print_usage();
@@ -59,19 +100,26 @@ parse_options (int argc, char **argv)
             case 'v':
                 print_version();
                 exit(EXIT_SUCCESS);
+            case 'u':
+                if (!optarg && NULL != argv[optind] && '-' != argv[optind][0]) {
+                    /* This construct is required to pass optarg
+                     * as -u path or --unix path, instead of
+                     * default getopt_long behavior that is
+                     * -upath or --unix=path.
+                     *
+                     * Test explaination
+                     * No argument is specified, and there is
+                     * another string in argv after this option
+                     * that does not look like an option
+                     */
+                    temp_arg = argv[optind++];
+                }
+                parse_unix_socket_param(temp_arg);
+                proto = UNIX;
+                break;
             default:
                 goto exit_failure;
         }
-    }
-
-    if (optind == argc-1) {
-        sock_path = argv[optind];
-    } else if (optind == argc) {
-        printf("%s: missing socket path\n", PROGRAM_NAME);
-        goto exit_failure;
-    } else {
-        printf("%s: two many arguments\n", PROGRAM_NAME);
-        goto exit_failure;
     }
 
     return;
@@ -93,15 +141,19 @@ main (int argc, char **argv)
 
     parse_options(argc, argv);
 
+    if (proto == NONE) {
+        printf("No protocol specified!\n");
+        exit(EXIT_FAILURE);
+    }
     /* Create Unix socket */
     sfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sfd == -1)
         handle_error("socket");
 
-    /* Initialize server_addr structure with pathname set to sock_path */
+    /* Initialize server_addr structure with pathname set to unix_sock_path */
     memset(&server_addr, 0, sizeof(struct sockaddr_un)); /* clear structure */
     server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, sock_path, sizeof(server_addr.sun_path)-1);
+    strncpy(server_addr.sun_path, unix_sock_path, sizeof(server_addr.sun_path)-1);
 
     /* Attempt connection to a socket binded to path_name */
     if (connect(sfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_un)) == -1)
