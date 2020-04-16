c33faccad201121b0cb3879058d54e26c90ed6b2
Fedor Chervyakov <fedor.chervyakov@emlid.com>
2020-04-17 01:23:31 +0300

    client: add tcp ping sender, update option parser
    client.c:
    - Remove protocol enum, use sock_domain instead.
    - Update Unix socket argument parser. Initialize unix_sock_path only
    there.
    - Add --connect|-C options, and update usage message correspondingly.
    - Add Inet socket argument parser.
    - Add check for conflicting protocols in parse_options.
    - Redirect error output prints to stderr.
    - Add socket domain switch to main. Since socket creation and
    connection process is different for inet and unix sockets,
    the switch is used.
    - Move unix socket creation, connection, and address structure
    initialization to AF_UNIX case of domain switch.
    - Add inet socket creation, connection, and address selection to AF_INET
    case of domain switch. Address selection is performed with the help of
    getaddrinfo, and the process is as described in getaddrinfo manpage.
    - Replace read with recv, and write with send for operations on sockets.
    This is useful in case we want to set flags. However, no flags are used.

--- src/client.c
+++ src/client.c
@@ -14,34 +14,37 @@
 #include <string.h>
 #include <getopt.h>
 
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
 #include "pingpong.h"
 
 
 #define PROGRAM_NAME "client"
 
 
-static char *short_options = "hvu::";
+static char *short_options = "u::C::hv";
 static struct option long_options[] = {
     {"unix",    optional_argument, 0, 'u'},
+    {"connect",  optional_argument, 0, 'C'},
     {"help",    no_argument,       0, 'h'},
     {"version", no_argument,       0, 'v'},
     {0,         0,                 0, 0}
 };
 
-enum protocol
-{
-    NONE,
-    UNIX,
-};
+static int sock_domain, sock_type;
+static char unix_sock_path[108];
+static char hostname[254];
+static char service[16];
 
-static enum protocol proto = NONE;
-
-static char unix_sock_path[108] = UNIX_SOCKET_PATH;
 
 static void print_version (void);
 static void print_usage (void);
 static void parse_options (int argc, char **argv);
-static void parse_unix_socket_param (const char *optarg);
+static void parse_unix_sock_addr (const char *optarg);
+static void parse_inet_sock_addr (const char *optarg);
+
 int main (int argc, char **argv);
 
 
@@ -58,28 +61,52 @@ print_usage (void)
     printf("%s: usage \n./%s [-hvu] [-u sock_path]\n",
            PROGRAM_NAME, PROGRAM_NAME);
     puts(("\
-\t-h, --help              show this message\n\
-\t-u, --unix [pathname]   use Unix socket, bind it to pathname\n\
-\t-v, --version           show version"));
+\t-h, --help                show this message\n\
+\t-u, --unix [pathname]     use Unix socket, connect it to pathname\n\
+\t-C, --connect [addr:port] use TCP socket, connect it to addr:port\n\
+\t-v, --version             show version"));
     return;
 }
 
-
 static void
-parse_unix_socket_param (const char *optarg)
+parse_unix_sock_addr (const char *optarg)
 {
     char *env_var;
 
-    if (! optarg) {
-        if ((env_var = getenv("UNIX_SOCKET")) != NULL) {
-            /* Use env var if set */
-            strcpy(unix_sock_path, env_var);
-        }
-        return;
+    /* Choose a path to connect to */
+    if (optarg) {
+        strcpy(unix_sock_path, optarg);
+    //} else if ((char *env_var = getenv("UNIX_SOCKET")) != NULL) {
+    } else if ((env_var = getenv("UNIX_SOCKET")) != NULL) {
+        /* Use env var if set */
+        strcpy(unix_sock_path, env_var);
+    } else {
+        strcpy(unix_sock_path, UNIX_SOCKET_PATH);
     }
 
-    strcpy(unix_sock_path, optarg);
     return;
+}
+
+static void
+parse_inet_sock_addr (const char *optarg)
+{
+    if (optarg) {
+        /* Locate last colon ":" in the optarg */
+        char *colon_loc = strrchr(optarg, ':');
+        
+        if (colon_loc != NULL) {
+            /* colon is present, assuming arg is in host:port format */
+            strncpy(hostname, optarg, colon_loc-optarg);
+            strcpy(service, colon_loc+1);
+        } else {
+            /* colon is not present */
+            fputs("parse_inet_sockaddr: use host:port format", stderr);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        strcpy(hostname, "localhost");
+        strcpy(service, INET_SOCKET_PORT);
+    }
 
 }
 
@@ -99,6 +126,11 @@ parse_options (int argc, char **argv)
                 print_version();
                 exit(EXIT_SUCCESS);
             case 'u':
+                if (sock_domain || sock_type) {
+                    fprintf(stderr, "%s: conficting options specified\n", argv[0]);
+                    goto exit_failure;
+                }
+
                 if (!optarg && NULL != argv[optind] && '-' != argv[optind][0]) {
                     /* This construct is required to pass optarg
                      * as -u path or --unix path, instead of
@@ -112,8 +144,29 @@ parse_options (int argc, char **argv)
                      */
                     temp_arg = argv[optind++];
                 }
-                parse_unix_socket_param(temp_arg);
-                proto = UNIX;
+
+                sock_domain = AF_UNIX;
+                sock_type = SOCK_STREAM;
+
+                parse_unix_sock_addr(temp_arg);
+
+                break;
+            case 'C':
+                if (sock_domain || sock_type) {
+                    fprintf(stderr, "%s: conficting options specified\n", argv[0]);
+                    goto exit_failure;
+                }
+
+                if (!optarg && NULL != argv[optind] && '-' != argv[optind][0]) {
+                    /* For the explanation, see the comment in the 'u' case */
+                    temp_arg = argv[optind++];
+                }
+
+                sock_domain = AF_INET;
+                sock_type = SOCK_STREAM;
+
+                parse_inet_sock_addr(temp_arg);
+
                 break;
             default:
                 goto exit_failure;
@@ -123,7 +176,7 @@ parse_options (int argc, char **argv)
     return;
 
 exit_failure:
-    printf("Try './%s -h' for more information.\n", PROGRAM_NAME);
+    fprintf(stderr, "Try './%s -h' for more information.\n", PROGRAM_NAME);
     exit(EXIT_FAILURE);
 }
 
@@ -131,7 +184,7 @@ int
 main (int argc, char **argv)
 {
     int sfd;
-    struct sockaddr_un server_addr;
+    struct addrinfo *rp;
 
     ssize_t read_cnt, write_cnt, write_cum;
     char recv_buf[BUF_SIZE];
@@ -139,40 +192,80 @@ main (int argc, char **argv)
 
     parse_options(argc, argv);
 
-    if (proto == NONE) {
-        printf("No protocol specified!\n");
-        exit(EXIT_FAILURE);
+    struct sockaddr_un unix_addr;
+
+    int err;
+    struct addrinfo *inet_result;
+    struct addrinfo hints;
+
+    /* Create and connect socket to the address */
+    switch (sock_domain) {
+        case AF_UNIX:
+            /* Initialize unix_addr structure */
+            memset(&unix_addr, 0, sizeof(struct sockaddr_un)); /* clear structure */
+            unix_addr.sun_family = AF_UNIX;
+            strncpy(unix_addr.sun_path, unix_sock_path, sizeof(unix_addr.sun_path)-1);
+
+            sfd = socket(sock_domain, sock_type, 0);
+            if (sfd == -1)
+                handle_error("socket");
+
+            if (connect(sfd, (struct sockaddr *) &unix_addr, sizeof(struct sockaddr_un)) == -1)
+                handle_error("connect");
+
+            break;
+        case AF_INET:
+
+            memset(&hints, 0, sizeof(struct addrinfo));
+            hints.ai_family = AF_UNSPEC;
+            hints.ai_socktype = sock_type;
+            hints.ai_protocol = 0;
+            hints.ai_canonname = NULL;
+            hints.ai_addr = NULL;
+            hints.ai_next = NULL;
+
+            if ((err=getaddrinfo(hostname, service, &hints, &inet_result)) != 0) {
+                fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+                exit(EXIT_FAILURE);
+            }
+
+            for (rp = inet_result; rp != NULL; rp = rp->ai_next) {
+                sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+                if (sfd == -1)
+                    continue;
+
+                if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+                    break;
+
+                close(sfd);
+            }
+
+            if (rp == NULL)
+                handle_error("connect");
+            break;
+        default:
+            fputs(("No protocol specified!\n"), stderr);
+            exit(EXIT_FAILURE);
     }
-    /* Create Unix socket */
-    sfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sfd == -1)
-        handle_error("socket");
 
-    /* Initialize server_addr structure with pathname set to unix_sock_path */
-    memset(&server_addr, 0, sizeof(struct sockaddr_un)); /* clear structure */
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, unix_sock_path, sizeof(server_addr.sun_path)-1);
-
-    /* Attempt connection to a socket binded to path_name */
-    if (connect(sfd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_un)) == -1)
-        handle_error("connect");
+    freeaddrinfo(inet_result);
 
     write_cnt = 0;
     printf("Sending %s\n", send_buf);
 
-    while ((write_cnt=write(sfd, send_buf+write_cum, strlen(send_buf)-write_cum)) < strlen(send_buf)-write_cum) {
+    while ((write_cnt=send(sfd, send_buf+write_cum, strlen(send_buf)-write_cum, 0)) < strlen(send_buf)-write_cum) {
         if (write_cnt == -1)
-            handle_error("write");
+            handle_error("send");
         write_cum += write_cnt;
     }
 
     do {
         /* Read the message to recv_buf */
-        read_cnt=read(sfd, recv_buf, BUF_SIZE);
+        read_cnt=recv(sfd, recv_buf, BUF_SIZE, 0);
         if (read_cnt == -1)
-            handle_error("read");
+            handle_error("recv");
 
-        /* Check if there is an unread data in the buffer */
+        /* Check if there is unread data in the buffer */
         if (ioctl(sfd, FIONREAD, &read_cnt) == -1)
             handle_error("ioctl");
 
